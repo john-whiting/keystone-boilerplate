13ab092670751f4dfad180ee548bd0598745f11a
John Whiting <thejwhiting@gmail.com>
2024-08-20 21:11:21 -0400

    chore: updated dependencies

--- keystone.ts
+++
@@ -1,7 +0,0 @@
-// Keystone requires a "keystone.ts" file in the project root.
-// This import and then export allows for the "src" directory
-// to be the source of truth.
-
-import keystoneApplication from "./src/index";
-
-export default keystoneApplication;
